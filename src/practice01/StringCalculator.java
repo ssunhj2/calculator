2f140d45a3fcfbc6d7378acbbedccba665f88e50
es2sun <wiiro017@gmail.com>
2020-07-20 22:46:20 +0900

    문자열 계산기 요구사항처리

--- src/practice01/StringCalculator.java
+++ src/practice01/StringCalculator.java
@@ -1,5 +1,8 @@
 package practice01;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 1. 전달하는 문자를 구분자로 분리한 후 각 숫자의 합을 구해 반환한다.
  * 2. 구분자는 , 또는 : 이다.
@@ -10,8 +13,49 @@ package practice01;
  */
 public class StringCalculator
 {
+    // 1. 구분자(, 또는 :)로 구분한 문자열을 전달하는 경우 구분자를 기준으로 각 숫자를 분리한다.
+    // 2. 분리한 각 숫자의 합을 반환한다.
+    // 3. 빈 문자열인 경우 0을 반환한다.
     int add(String text)
     {
-        return 0;
+        // 빈문자열 또는 null인 경우 0을 반환한다.
+        if(text == null) return 0;
+        if(text.isEmpty()) return 0;
+
+        String[] nums = text.split(",|:");
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if(m.find())
+        {
+            String customDevide = m.group(1);
+            nums = m.group(2).split(customDevide);
+        }
+
+        int result = 0;
+        for(int i=0; i< nums.length; i++)
+        {
+            try
+            {
+                String num = nums[i];
+                if(" ".equals(num)) num = "0";
+                int number = Integer.parseInt(num);
+
+                if(number < 0)
+                {
+                    new RuntimeException();
+                    break;
+                }
+
+                result += number;
+            }
+            catch (RuntimeException e)
+            {
+                System.out.println("양수값을 입력해주세요.");
+            }
+        }
+
+        return result;
     }
+
+
 }
--- test/practice01/StringCalculatorTest.java
+++ test/practice01/StringCalculatorTest.java
@@ -1,8 +1,13 @@
 package practice01;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class StringCalculatorTest
 {
-
+    @Test
+    public void add()
+    {
+        StringCalculator strCal = new StringCalculator();
+        System.out.println(strCal.add("//,\n1,2,3"));
+    }
 }
